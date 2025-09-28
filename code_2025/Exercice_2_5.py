37b6c2b1366f24f971138ff78d278084e30a1f0f
ah2310 <alexander.hilbert@outlook.com>
2025-09-28 17:00:07 +0200

    Add files via upload

---
+++ code_2025/Exercice_2_5.py
@@ -0,0 +1,11 @@
+# LAST CHANGED : 2025/09/28
+# Exercise 2.5 - page 23
+
+
+n1 = int(input("Enter a number: "))
+
+n2 = int(input("Enter another number "))
+
+print("Somme: ", n1 + n2)
+
+print("Produit ", n1 * n2)
---
+++ code_2025/Exercice_2_6.py
@@ -0,0 +1,12 @@
+# LAST CHANGED : 2025/09/28
+# Exercise 2.6 - page 23
+
+n1 = int(input("Enter a number: "))
+
+n2 = int(input("Enter another number "))
+
+n1,n2 = n2,n1
+
+print("n1 : ", n1)
+print("n2 : ", n2)
+
