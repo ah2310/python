9932cf2a08d5af89df59d8ab1457e0a5fe05ffe8
ah2310 <alexander.hilbert@outlook.com>
2025-10-01 23:08:35 +0200

    Add files via upload

---
+++ code_2025/Exercice_2_7.py
@@ -0,0 +1,14 @@
+# LAST CHANGED : 2025/09/28
+# Exercise 2.7 - page 23
+
+# Import statistics Library
+import statistics
+
+n1  = 10
+n2  = 50
+n3  = 30
+
+ 
+print("Moyenne : ", statistics.mean([n1, n2, n3]))
+
+print("Moyenne : ", (n1 + n2 + n3) / 3)
--- code_2025/Exercice_2_8.py
+++ code_2025/Exercice_2_8.py
@@ -9,7 +9,7 @@ n2  = 20
 n3  = 30
 
  
-print("Moyenne harmonique: ", statistics.harmonic_mean([10, 20, 30]))
+print("Moyenne harmonique: ", statistics.harmonic_mean([n1, n2, n3]))
 
 
 print("Moyenne harmonique: ", 3 / (1/n1 + 1/n2 + 1/n3))
