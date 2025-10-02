0f9aa63e5be1b179833d55da2c913e991b412a84
ah2310 <alexander.hilbert@outlook.com>
2025-10-02 09:27:55 +0200

    Update Exercice_2_7.py

--- code_2025/Exercice_2_7.py
+++ code_2025/Exercice_2_7.py
@@ -8,7 +8,8 @@ n1  = 10
 n2  = 50
 n3  = 30
 
- 
+# Using Python statistics library  
 print("Moyenne : ", statistics.mean([n1, n2, n3]))
 
+# Doing simple calculation
 print("Moyenne : ", (n1 + n2 + n3) / 3)
