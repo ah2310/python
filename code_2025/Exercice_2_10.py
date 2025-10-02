9f3fb5f3ca1545206a745011885710e86bc33443
ah2310 <alexander.hilbert@outlook.com>
2025-10-02 09:37:55 +0200

    Add files via upload

---
+++ code_2025/Exercice_2_10.py
@@ -0,0 +1,12 @@
+# LAST CHANGED : 2025/10/01
+# Exercise 2.10 - page 23
+
+
+r1 = int(input("Enter value for R1: "))
+r2 = int(input("Enter value for R2: "))
+r3 = int(input("Enter value for R3: "))
+r4 = int(input("Enter value for R4: "))
+
+# Calculate result
+r_result = 1 / (1/r1 + 1/r2 + 1/r3 + 1/r4) 
+print("Result: ", r_result)
