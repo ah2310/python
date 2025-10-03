c1c60002c53af6bcc50fc38a165976167edd0a03
ah2310 <alexander.hilbert@outlook.com>
2025-10-03 17:40:39 +0200

    Add files via upload

---
+++ code_2025/Exercice_2_12.py
@@ -0,0 +1,16 @@
+# LAST CHANGED : 2025/10/03
+# Exercise 2.12 - page 24
+
+
+dividende = int(input("dividende: "))
+diviseur  = int(input("diviseur: "))
+
+quotient  = dividende // diviseur
+reste     = dividende % diviseur
+
+print("-----------------------------")
+print(f"{dividende} = {diviseur} * {quotient} + {reste}")
+
+
+
+
