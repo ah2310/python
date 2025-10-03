005b7c4bcd05bafe62ff5ce967352331d2cf5ffa
ah2310 <alexander.hilbert@outlook.com>
2025-10-03 17:32:55 +0200

    Update Exercice_2_13.py

--- code_2025/Exercice_2_13.py
+++ code_2025/Exercice_2_13.py
@@ -1,20 +1,19 @@
 # LAST CHANGED : 2025/10/01
 # Exercise 2.13 - page 24
 
-
 first_term = int(input("first term: "))
 common_difference = int(input("common difference: "))
 n = int(input("n: "))
 
-
+sequence = ""
 
 for x in range(1, n+1):
-    print("Term [", x, "] : ", first_term)
-    result = first_term
+    n_term = first_term
+    sequence += (str(first_term) + " ")
     first_term += common_difference
     
-   
-    
 print("-----------------------------")
-print("n-th number of sequence: ", result)
+print("sequence                 : ", sequence)
+print(f"{n}-th number of sequence : ", n_term)
+    
     
